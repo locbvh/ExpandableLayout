006b0f719587274b2c6a0c472e341d584610c36d
Loc Bui <bvhloc@gmail.com>
2016-01-21 11:47:22 +0700

    publish

---
+++ app/src/main/java/com/bvhloc/expandablelayout/ExpandableLayoutListenerAdapter.java
@@ -0,0 +1,40 @@
+package com.bvhloc.expandablelayout;
+
+public abstract class ExpandableLayoutListenerAdapter implements ExpandableLayoutListener {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onAnimationStart() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void onAnimationEnd() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void onPreOpen() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void onPreClose() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void onOpened() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void onClosed() {
+    }
+}
